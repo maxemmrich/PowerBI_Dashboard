fc4b4a7db9340f2f1a1bab589cdf876ff0a1d3df
maxemmrich <73600044+maxemmrich@users.noreply.github.com>
2021-04-30 20:13:36 +0200

    Add files via upload

---
+++ FACT_InternetSales.sql
@@ -0,0 +1,34 @@
+-- Cleansed FACT_InternetSales Table --
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  --  ,[PromotionKey]
+  --  ,[CurrencyKey]
+  --  ,[SalesTerritoryKey]
+  [SalesOrderNumber], 
+  --  [SalesOrderLineNumber], 
+  --  ,[RevisionNumber]
+  --  ,[OrderQuantity], 
+  --  ,[UnitPrice], 
+  --  ,[ExtendedAmount]
+  --  ,[UnitPriceDiscountPct]
+  --  ,[DiscountAmount] 
+  --  ,[ProductStandardCost]
+  --  ,[TotalProductCost] 
+  [SalesAmount] 
+  --  ,[TaxAmt]
+  --  ,[Freight]
+  --  ,[CarrierTrackingNumber] 
+  --  ,[CustomerPONumber] 
+  --  ,[OrderDate] 
+  --  ,[DueDate] 
+  --  ,[ShipDate] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2 -- Extract Year from DateKey and limit results to past 2 years
+ORDER BY
+  OrderDateKey ASC
